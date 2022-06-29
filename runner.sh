e021ca6f3cb0fd1d64906c66b6f8b60fb8286fd7
Andrea Montibeller <45923370+AMontiB@users.noreply.github.com>
2022-06-29 10:56:09 +0200

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,5 +1,5 @@
 #FOR H1
 echo "running H1 hypothesis"
-nohup python -u main_H1_ds.py --videos PATH_TO_VIDEOS --fingerprint PATH_TO_FINGERPRINTS --gpu_dev /gpu:N >| output_H1.log & #N is the GPU_ID, if you have just 1 GPU N=0
+nohup python -u main_H1_ds.py --videos PATH_TO_VIDEOS --fingerprint PATH_TO_FINGERPRINTS --output PATH_TO_OUTPUT_FOLDER --gpu_dev /gpu:N >| output_H1.log & 
 echo "running H0 hypothesis"
-nohup python -u main_H0_ds.py --videos PATH_TO_VIDEOS --fingerprint PATH_TO_FINGERPRINTS --gpu_dev /gpu:N >| output_H0.log & #N is the GPU_ID, if you have just 1 GPU N=0
+nohup python -u main_H0_ds.py --videos PATH_TO_VIDEOS --fingerprint PATH_TO_FINGERPRINTS --output PATH_TO_OUTPUT_FOLDER --gpu_dev /gpu:N >| output_H0.log & 
